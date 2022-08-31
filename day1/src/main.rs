4db315db53d41ee169b6307886d0773b3a11995b
egodfrey19 <73030617+egodfrey19@users.noreply.github.com>
2022-08-31 19:22:48 +0100

    solution for day 1

---
+++ day1/src/main.rs
@@ -0,0 +1,36 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn solve(measurements: &[u32]) -> u32 {
+    (0..measurements.len() - 1)
+        .map(|i| (measurements[i], measurements[i + 1]))
+        .filter(|(x, y)| x < y)
+        .fold(0, |count, _ele| count + 1)
+}
+
+fn solve2(measurements: &[u32]) -> u32 {
+    // It's probably slightly more efficient not to construct the intermediate vector
+    // An easy-ish way to do that would be to change solve to be able to take in an IntoIterator
+    // instead, but I'm not sure how to iterate over pairs of elements.
+    let sliding_measurements: Vec<u32> = (0..measurements.len() - 2)
+        .map(|i| (measurements[i], measurements[i + 1], measurements[i + 2]))
+        .map(|(x, y, z)| x + y + z)
+        .collect();
+
+    solve(&sliding_measurements)
+}
+
+fn main() {
+    let mut file = File::open("input").expect("Failed to open input file");
+    let mut s = String::new();
+    file.read_to_string(&mut s)
+        .expect("Failed to read from file");
+
+    let input: Vec<u32> = s
+        .split('\n')
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse::<u32>().expect("failed to parse as integer"))
+        .collect();
+    println!("Solution for part 1: {}", solve(&input));
+    println!("Solution for part 2: {}", solve2(&input));
+}
